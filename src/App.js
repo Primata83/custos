93e596ff4fd49383acd110f5e3a45eff7cd0a0d7
Hudson Cleiton <hudsoncleiton01@gmail.com>
2024-10-02 10:57:59 -0300

    bora salvar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/newproject">NewProject</Link>
+        </div>
+        
+        <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/contact" exact element={<Contact/>}/>
+          <Route path="/Company" exact element={<Company/>}/>
+          <Route path="/NewProject" exact element={<NewProject/>}/>
+        </Routes>
+        <p>Footer</p>
+    </Router>  
+    
+
+  )
 }
 
-export default App;
+export default App
