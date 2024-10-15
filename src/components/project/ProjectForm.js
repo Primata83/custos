17e22696400aed3353a57f0a36c8107c6e7502c3
Hudson Cleiton <hudsoncleiton01@gmail.com>
2024-10-15 10:12:35 -0300

    foi

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import {useState, useEffect} from 'react'
 import Input from '../form/Input'
 import styles from './ProjectForm.module.css'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
 function ProjectForm({btnText}) {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((resp) => resp.jason())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
            <Input
@@ -18,7 +35,11 @@ function ProjectForm({btnText}) {
                 name="budget"
                 placeholder="Insira o nome do projeto"
             />
-            <Select name="category_id" text="Selecione a categoria"/>
+            <Select 
+                name="category_id" 
+                text="Selecione a categoria"
+                options={categories}
+            />
             <SubmitButton text={btnText}/>
         </form>
     )
