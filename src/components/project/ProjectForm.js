6051bbbf4cd1db04f17ce48393bfc7db701eaaf9
Hudson Cleiton <hudsoncleiton01@gmail.com>
2024-10-22 11:19:55 -0300

    deu certo, mas vamos ver amanha

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,12 +1,10 @@
-import {useEffect, useState} from 'react'
-
+import { useState, useEffect } from 'react'
 import Input from '../form/Input'
+import styles from './ProjectForm.module.css'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
-import styles from './ProjectForm.module.css'
 
-function ProjectForm({ handleSubmit, btnText, projectData }){
-
+function ProjectForm({ handleSubmit, btnText, projectData }) {
     const [categories, setCategories] = useState([])
     const [project, setProject] = useState(projectData || {})
 
@@ -14,14 +12,17 @@ function ProjectForm({ handleSubmit, btnText, projectData }){
         fetch("http://localhost:5000/categories", {
             method: "GET",
             headers: {
-                'Content-type': 'application/json'
-            }
+                'Content-Type': 'application/json',
+            },
         })
-            .then((resp) => resp.json())
-            .then((data) => {
-                setCategories(data)
-            })
-            .catch((err) => console.log(err))
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log("Categorias carregadas:", data)  // Verifique os dados aqui
+            setCategories(data)
+        })
+        .catch((err) => {
+            console.error("Erro ao carregar categorias:", err)
+        })
     }, [])
 
     const submit = (e) => {
@@ -29,13 +30,13 @@ function ProjectForm({ handleSubmit, btnText, projectData }){
         handleSubmit(project)
     }
 
-    function handleChange(e){
+    function handleChange(e) {
         setProject({ ...project, [e.target.name]: e.target.value })
     }
 
-    function handleCategory(e){
-        setProject({ 
-            ...project, 
+    function handleCategory(e) {
+        setProject({
+            ...project,
             category: {
                 id: e.target.value,
                 name: e.target.options[e.target.selectedIndex].text,
@@ -45,30 +46,30 @@ function ProjectForm({ handleSubmit, btnText, projectData }){
 
     return (
         <form onSubmit={submit} className={styles.form}>
-            <Input 
-                type="text" 
-                text="Nome do projeto" 
-                name="name" 
+            <Input
+                type="text"
+                text="Nome do projeto"
+                name="name"
                 placeholder="Insira o nome do projeto"
                 handleOnChange={handleChange}
                 value={project.name ? project.name : ''}
             />
-            <Input 
-                type="number" 
-                text="Orçamento do projeto" 
-                name="budget" 
-                placeholder="Insira o orçamento total"
+            <Input
+                type="number"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento do projeto"
                 handleOnChange={handleChange}
                 value={project.budget ? project.budget : ''}
             />
-            <Select 
-                name="category_id" 
-                text="Selecione a categoria" 
+            <Select
+                name="category_id"
+                text="Selecione a categoria"
                 options={categories}
                 handleOnChange={handleCategory}
                 value={project.category ? project.category.id : ''}
             />
-            <SubmitButton text={btnText}/>
+            <SubmitButton text={btnText} />
         </form>
     )
 }
