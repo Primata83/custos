7618cead25e86486593b40ac221b7cba2edb3283
Hudson Cleiton <hudsoncleiton01@gmail.com>
2024-11-26 12:10:48 -0300

    Updates

---
+++ precache-manifest.f9fd8830c8f149b6ffa46a92aef2395f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88b882ab4efd34cc3925557fd3fb35c",
+    "url": "/custos/index.html"
+  },
+  {
+    "revision": "eaa6227c521b73edae2b",
+    "url": "/custos/static/css/main.9bf1e4ae.chunk.css"
+  },
+  {
+    "revision": "42d31e1c7434ba954e0f",
+    "url": "/custos/static/js/2.727ce389.chunk.js"
+  },
+  {
+    "revision": "a70cfba45799f8a4deb5",
+    "url": "/custos/static/js/3.cf2aeaea.chunk.js"
+  },
+  {
+    "revision": "eaa6227c521b73edae2b",
+    "url": "/custos/static/js/main.ef861f06.chunk.js"
+  },
+  {
+    "revision": "38c42007f0077faa1076",
+    "url": "/custos/static/js/runtime~main.df517a03.js"
+  },
+  {
+    "revision": "6ff46bbfc1053f20778eaf70d506d7e2",
+    "url": "/custos/static/media/custos.6ff46bbf.png"
+  },
+  {
+    "revision": "37833178f89c1b40c84eb98545b2abcd",
+    "url": "/custos/static/media/loading.37833178.svg"
+  },
+  {
+    "revision": "393ed0c7a46291aa86d4f7587a191db3",
+    "url": "/custos/static/media/moeda.393ed0c7.png"
+  }
+]);
